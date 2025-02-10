4c406bbca6459e26101fb509a802d13991ffb5d9
6berNova <6berNova@gmail.com>
2025-02-10 17:32:27 +0100

    Ajout du jeu Flappy Bird

---
+++ ServiceWorker.js
@@ -0,0 +1,40 @@
+const cacheName = "CyberNova-Flappy bird-0.1";
+const contentToCache = [
+<<<<<<< HEAD
+    "Build/Build 1.1 - Ajout Pause.loader.js",
+    "Build/Build 1.1 - Ajout Pause.framework.js",
+    "Build/Build 1.1 - Ajout Pause.data",
+    "Build/Build 1.1 - Ajout Pause.wasm",
+=======
+    "Build/Build for phone.loader.js",
+    "Build/Build for phone.framework.js",
+    "Build/Build for phone.data",
+    "Build/Build for phone.wasm",
+>>>>>>> 72ea54c (Initial commit)
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
